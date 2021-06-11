42aeff32aeadc5be7aa86b091f4346ff89a5667c
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-11 19:52:31 +0100

    create SelectInput

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="nes-select py-2">
+        <select :value="value" @input="$emit('input', $event.target.value)">
+            <option disabled :value="null">{{ placeholder }}</option>
+            <option :v-for="option in options" :value="option.id" :selected="value === option.id">{{ option.value }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        placeholder: {
+            type: String,
+            default: 'Select Option'
+        },
+        options: {
+            type: Array,
+            default: () => []
+        },
+        value: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
