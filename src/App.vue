1523c48fc57a99df5c76fdfc1d70470e54b17404
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-11 09:30:55 +0100

    mockup design

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link
-      :to="{ name: 'home' }"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-      custom
-    >
-      <nav-option
-        :href="href"
-        :is-active="isActive"
-        :is-exact-active="isExactActive"
-        :navigate="navigate"
-        title="Wallet"
-      />
-    </router-link> |
-    <router-link
-      :to="{ name: 'about' }"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-      custom
-    >
-      <nav-option
-        :href="href"
-        :is-active="isActive"
-        :is-exact-active="isExactActive"
-        :navigate="navigate"
-        title="About"
-      />
-    </router-link>
+  <div class="p-5 mx-auto" style="max-width: 900px">
+    <div id="nav">
+      <router-link
+        :to="{ name: 'home' }"
+        v-slot="{ href, navigate, isActive, isExactActive }"
+        custom
+      >
+        <nav-option
+          :href="href"
+          :is-active="isActive"
+          :is-exact-active="isExactActive"
+          :navigate="navigate"
+          title="Wallet"
+        />
+      </router-link> |
+      <router-link
+        :to="{ name: 'protocol' }"
+        v-slot="{ href, navigate, isActive, isExactActive }"
+        custom
+      >
+        <nav-option
+          :href="href"
+          :is-active="isActive"
+          :is-exact-active="isExactActive"
+          :navigate="navigate"
+          title="Protocols"
+        />
+      </router-link>
+    </div>
+    <div class="py-5">
+      <router-view/>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" class="nes-avatar is-large"  style="image-rendering: pixelated;" src="../assets/logo.png">
-    <img class="nes-avatar is-rounded is-large" alt="Gravatar image example" src="https://www.gravatar.com/avatar?s=15" style="image-rendering: pixelated;">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="nes-container with-title">
+    <p class="title">
+      Search Criteria
+    </p>
+    <div class="row align-items-center">
+      <div class="col-sm">
+        <div class="nes-select py-2">
+          <select required="" id="default_select">
+            <option value="" disabled="" selected="" hidden="">Chain</option>
+            <option value="0">To be</option>
+            <option value="1">Not to be</option>
+          </select>
+        </div>
+      </div>
+      <div class="col-sm py-2">
+        <div class="nes-field">
+          <input type="text" id="name_field" placeholder="Address" class="nes-input">
+        </div>
+      </div>
+    </div>
+    <div class="row align-items-center">
+      <div class="col-sm py-2">
+        <label>
+          <input type="checkbox" class="nes-checkbox" checked />
+          <span>Show Balances</span>
+        </label>
+      </div>
+      <div class="col-sm py-2">
+        <button type="button" class="nes-btn is-primary">Search</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="my-5"></div>
+  <div class="nes-container with-title">
+    <p class="title">
+      <img class="nes-avatar is-rounded" alt="Gravatar image example" src="https://www.covalenthq.com/static/images/icons/display-icons/fantom-ftm-logo.png" style="image-rendering: pixelated;">
+      TXs
+    </p>
+    <div class="nes-table-responsive">
+      <table class="nes-table is-bordered is-centered">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Status</th>
+            <th>When</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>In</td>
+            <td>1.3782</td>
+            <td>
+              <span class="nes-text is-success">Success</span>
+            </td>
+            <td>2 Jun 21, 17:37</td>
+            <td>
+              <button type="button" class="nes-btn is-primary" onclick="document.getElementById('dialog-default').showModal();">
+                View
+              </button>
+              <dialog class="nes-dialog is-rounded" id="dialog-default">
+                <form method="dialog">
+                  <p class="title">Dialog</p>
+                  <p>Alert: this is a dialog.</p>
+                  <menu class="dialog-menu">
+                    <button class="nes-btn">Cancel</button>
+                    <button class="nes-btn is-primary">Confirm</button>
+                  </menu>
+                </form>
+              </dialog>
+            </td>
+          </tr>
+          <tr>
+            <td>Out</td>
+            <td>5.01231</td>
+            <td>
+              <span class="nes-text is-error">Fail</span>
+            </td>
+            <td>2 Jun 21, 17:37</td>
+            <td>
+              <button type="button" class="nes-btn is-primary" onclick="document.getElementById('dialog-default').showModal();">
+                View
+              </button>
+              <dialog class="nes-dialog is-rounded" id="dialog-default">
+                <form method="dialog">
+                  <p class="title">Dialog</p>
+                  <p>Alert: this is a dialog.</p>
+                  <menu class="dialog-menu">
+                    <button class="nes-btn">Cancel</button>
+                    <button class="nes-btn is-primary">Confirm</button>
+                  </menu>
+                </form>
+              </dialog>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
