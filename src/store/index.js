5c9efd92dc9bb99cba16c26af7eab6d3a8ec3088
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-13 02:03:23 +0100

    show transactions by default

--- src/store/index.js
+++ src/store/index.js
@@ -24,7 +24,7 @@ export default new Vuex.Store({
                         value: '',
                         error: ''
                     },
-                    showTransactions: false,
+                    showTransactions: true,
                     loading: false,
                     responseError: ''
                 },
