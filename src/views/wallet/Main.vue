194d7ff6c3f6de3b18e93a01773f91871f7824bc
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-13 01:07:49 +0100

    create Balances component

--- src/views/wallet/Main.vue
+++ src/views/wallet/Main.vue
@@ -12,44 +12,7 @@
         </p>
 
         <div v-else-if="showResponseContainer" class="my-5">
-            <container title="Balances">
-                <div class="row align-items-center">
-                    <div class="col">
-                        <p class="my-0">Total: ${{ totalBalance }}</p>
-                    </div>
-                    <div class="col text-end">
-                        <button
-                            type="button"
-                            class="nes-btn is-primary"
-                            @click="expanded = !expanded"
-                        >{{ expanded ? 'Hide' : 'Expand' }} Details</button>
-                    </div>
-                </div>
-                <div v-show="expanded" class="nes-table-responsive mt-4">
-                    <table class="nes-table is-bordered is-centered">
-                        <thead>
-                            <tr>
-                            <th>Name</th>
-                            <th>Amount</th>
-                            <th>Balance</th>
-                            <th>History</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="item in wallet.balances.items" :key="item.contract_address">
-                                <td>{{ item.contract_name }} ({{ item.contract_ticker_symbol }})</td>
-                                <td>{{ item.balance_formatted }}</td>
-                                <td>${{ item.quote_formatted }}</td>
-                                <td class="text-center">
-                                    <button type="button" class="nes-btn is-primary" onclick="document.getElementById('dialog-default').showModal();">
-                                    View
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </container>
+            <balances/>
 
             <div class="my-5"></div>
 
@@ -122,22 +85,17 @@
 
 <script>
 import SearchCriteria from './SearchCriteria.vue';
+import Balances from './Balances.vue';
 import IconLoading from '../../components/IconLoading.vue';
 import Container from '../../components/Container.vue';
 import {mapState, mapGetters} from 'vuex';
-import {formatFiatValue} from '../../lib/helpers';
 
 export default {
-    data() {
-        return {
-            expanded: false 
-        }
-    },
     computed: {
         ...mapState(['app']),
         ...mapGetters(['wallet']),
         networkLogo() {
-            const chainId = this.app.wallet.form.chainId.value;
+            const chainId = this.wallet.form.chainId.value;
             if (chainId) {
                 return this.app.networks[chainId].logo_url;
             }
@@ -148,13 +106,11 @@ export default {
                 return this.wallet.balances.items !== null && this.wallet.transactions.items !== null;
             }
             return this.wallet.balances.items !== null;
-        },
-        totalBalance() {
-            return formatFiatValue(this.wallet.balances.total);
         }
     },
     components: {
         SearchCriteria,
+        Balances,
         Container,
         IconLoading
     }
