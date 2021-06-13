caf11cd159a16ff317e25707d3eebbee8c6e7f3f
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-13 17:25:31 +0100

    init restructuring

--- src/store/index.js
+++ src/store/index.js
@@ -1,108 +1,60 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {CHAINS, TOKEN_BALANCES, TRANSACTIONS} from '../config/endpoints';
+import {CHAINS} from '../config/endpoints';
 import {MAINNET_IDS} from '../config/supported_chains';
-import {formatTokenBalance, formatFiatValue} from '../lib/helpers';
+// import {formatTokenBalance, formatFiatValue} from '../lib/helpers';
 import axios from '../lib/axios';
-import { vsprintf } from 'sprintf-js';
-import moment from 'moment';
+// import { vsprintf } from 'sprintf-js';
+// import moment from 'moment';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        app: {
-            networks: {},
-            loading: true,
-            wallet: {
-                form: {
-                    chainId: {
-                        value: '',
-                        error: ''
-                    },
-                    address: {
-                        value: '',
-                        error: ''
-                    },
-                    showTransactions: true,
-                    loading: false,
-                    responseError: ''
-                },
-                balances: {
-                    items: null,
-                    total: 0
-                },
-                transactions: {
-                    items: null,
-                    visible: false
-                }
-            }
+        networks: {},
+        wallet: {},
+        loading: true,
+        form: {
+            chainId: {
+                value: '',
+                error: ''
+            },
+            address: {
+                value: '',
+                error: ''
+            },
+            loading: false,
+            responseError: ''
         }
     },
     getters: {
-        networkOptions({ app }) {
-            return Object.keys(app.networks).map(id => {
-                return {
-                    value: id,
-                    title: app.networks[id].label
-                }
+        networkOptions({ networks }) {
+            return Object.keys(networks).map(item => {
+                return {...item};
             });
-        },
-        wallet({ app }) {
-            return app.wallet;
         }
     },
     mutations: {
-        addNetworks({ app }, payload) {
+        addNetworks(state, payload) {
             payload.forEach(({chain_id, logo_url, label}) => {
                 // for now, we only want to process the mainnet chain ids
                 if (MAINNET_IDS.includes(parseInt(chain_id))) {
-                    Vue.set(app.networks, chain_id, { logo_url, label });
+                    Vue.set(state.networks, chain_id, { logo_url, label });
                 }
             });
         },
-        updateFormField({ app }, { section, field, payload }) {
+        updateFormField({ form }, { field, payload }) {
             if (typeof payload === 'boolean' || typeof payload === 'string') {
-                app[section].form[field] = payload;
+                form[field] = payload;
             } else {
-                app[section].form[field] = {
-                    ...app[section].form[field],
+                form[field] = {
+                    ...form[field],
                     ...payload
                 }
             }
         },
-        addBalances({ app }, payload) {
-            app.wallet.balances.total = 0;
-            app.wallet.balances.items = payload.map(item => {
-                app.wallet.balances.total += item.quote;
-                return {
-                    ...item,
-                    balance_formatted: formatTokenBalance(item.balance, item.contract_decimals),
-                    quote_formatted: formatFiatValue(item.quote)
-                }
-            });
-        },
-        addTransactions({ app }, payload) {
-            app.wallet.transactions.items = payload.map(item => {
-                return {
-                    ...item,
-                    type: item.from_address.toLowerCase() === app.wallet.form.address.value.toLowerCase() ? 'Out' : 'In',
-                    balance_formatted: formatTokenBalance(item.value, 18),
-                    quote_formatted: formatFiatValue(item.quote),
-                    created_at: moment.utc(item.block_signed_at).local().format('Do MMM YYYY, HH:mm')
-                }
-            });
-            app.wallet.transactions.visible = true;
-        },
-        resetWallet({ app }) {
-            app.wallet.form.responseError = '';
-            app.wallet.balances.items = null;
-            app.wallet.balances.total = 0;
-            app.wallet.transactions.items = null;
-            app.wallet.transactions.visible = false;
-        },
-        toggleAppLoading({ app }, payload) {
-            app.loading = payload;
+        toggleAppLoading(state, payload) {
+            state.loading = payload;
         }
     },
     actions: {
@@ -114,40 +66,6 @@ export default new Vuex.Store({
                 commit('addNetworks', response.data.data.items);
             }
             commit('toggleAppLoading', false);
-        },
-        async fetchWalletInfo({ commit }, { chainId, address, showTransactions }) {
-            commit('updateFormField', {
-                section: 'wallet',
-                field: 'loading',
-                payload: true
-            });
-
-            // reset wallet attributes
-            commit('resetWallet')
-
-            try {
-                // get balances
-                const balances = await axios.get(vsprintf(TOKEN_BALANCES, [chainId, address]));
-                commit('addBalances', balances.data.data.items);
-
-                // get transactions if selected
-                if (showTransactions) {
-                    const transactions = await axios.get(vsprintf(TRANSACTIONS, [chainId, address]));
-                    commit('addTransactions', transactions.data.data.items);
-                }
-            } catch (error) {
-                commit('updateFormField', {
-                    section: 'wallet',
-                    field: 'responseError',
-                    payload: error.response.data.error_message
-                });
-            } finally {
-                commit('updateFormField', {
-                    section: 'wallet',
-                    field: 'loading',
-                    payload: false
-                });
-            }
         }
     },
     modules: {
---
+++ src/views/home/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex';
+
+export default {
+    computed: {
+        ...mapState(['app']),
+        ...mapGetters(['wallet']),
+        networkLogo() {
+            const chainId = this.wallet.form.chainId.value;
+            if (chainId) {
+                return this.app.networks[chainId].logo_url;
+            }
+            return '';
+        },
+        showResponseContainer() {
+            if (this.wallet.transactions.visible) {
+                return this.wallet.balances.items !== null && this.wallet.transactions.items !== null;
+            }
+            return this.wallet.balances.items !== null;
+        }
+    },
+    components: {
+    }
+}
+</script>
