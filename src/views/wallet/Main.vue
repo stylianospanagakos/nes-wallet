05c74b12f000a56e135549ac23a784bdf884712f
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-16 16:36:40 +0100

    init token design update

--- src/views/wallet/Main.vue
+++ src/views/wallet/Main.vue
@@ -22,6 +22,29 @@
                 />
             </div>
             <div v-else class="text-center mt-5">
+                <container title="FTM">
+                    <div class="row align-items-center">
+                        <div class="col-md-6 text-start">
+                            <p class="my-0">Balance: 12313</p>
+                            <small class="d-block text-muted my-2">Fiat Value: $12312.42</small>
+                            <div>
+                            <span class="nes-badge is-splited w-auto">
+                                <span class="is-dark w-auto position-relative px-1">erc</span>
+                                <span class="is-success w-auto position-relative px-1">20</span>
+                            </span>
+                            <span class="nes-badge is-splited w-auto ms-3">
+                                <span class="is-dark w-auto position-relative px-1">erc</span>
+                                <span class="is-success w-auto position-relative px-1">271</span>
+                            </span>
+                            </div>
+                        </div>
+                        <div class="col-md-6 text-end">
+                            <a href="#" class="nes-badge w-auto">
+                                <span class="is-success position-relative">cryptocurrency</span>
+                            </a>
+                        </div>
+                    </div>
+                </container>
                 <p>Apologies, we couldn't fetch your wallet details.</p>
                 <action-button
                     @click="fetchBalanceHistory({chainId: wallet.chain_id, address: wallet.address.full})"
@@ -41,16 +64,17 @@
 <script>
 import ActionButton from '../../components/ActionButton.vue';
 import IconLoading from '../../components/IconLoading.vue';
+import Container from '../../components/Container.vue';
 import Token from './Token.vue';
 import {mapState, mapGetters, mapActions} from 'vuex';
 
 export default {
     created() {
         if (this.wallet) {
-            this.fetchBalanceHistory({
-                chainId: this.wallet.chain_id,
-                address: this.wallet.address.full
-            });
+            // this.fetchBalanceHistory({
+            //     chainId: this.wallet.chain_id,
+            //     address: this.wallet.address.full
+            // });
         }
     },
     computed: {
@@ -64,6 +88,7 @@ export default {
         ...mapActions(['fetchBalanceHistory'])
     },
     components: {
+        Container,
         ActionButton,
         IconLoading,
         Token
--- src/views/wallet/Token.vue
+++ src/views/wallet/Token.vue
@@ -4,24 +4,16 @@
             <div class="col-md-6">
                 <p class="my-0">Balance: {{ data.balance }}</p>
                 <small class="d-block text-muted my-2">{{ data.contract_address.truncated }}</small>
-                <action-button class="mt-3">Show Transfers</action-button>
-            </div>
-            <div v-if="data.history.line.length" class="col-md-6 text-end">
-                <line-graph
-                    label="Balance"
-                    :data="data.history.line"
-                    main-color="#5578eb"
-                    alt-color="#e1edd9"
-                />
-                <candle-chart
-                    label="Balance"
-                    up-color="#5578eb"
-                    down-color="#e1edd9"
-                    :data="data.history.candle"
-                />
-            </div>
-            <div v-else class="col-md-6 text-end">
-                <p class="nes-text is-disabled">No available data</p>
+                <div>
+                    <span class="nes-badge is-splited w-auto">
+                        <span class="is-dark w-auto position-relative px-1">erc</span>
+                        <span class="is-success w-auto position-relative px-1">20</span>
+                    </span>
+                    <span class="nes-badge is-splited w-auto ms-3">
+                        <span class="is-dark w-auto position-relative px-1">erc</span>
+                        <span class="is-success w-auto position-relative px-1">271</span>
+                    </span>
+                </div>
             </div>
         </div>
     </container>
@@ -29,9 +21,6 @@
 
 <script>
 import Container from '../../components/Container.vue';
-import LineGraph from '../../components/graphs/LineGraph.vue';
-import CandleChart from '../../components/graphs/CandleChart.vue';
-import ActionButton from '../../components/ActionButton.vue';
 import {mapMutations} from 'vuex';
 
 export default {
@@ -51,10 +40,7 @@ export default {
         }
     },
     components: {
-        Container,
-        LineGraph,
-        CandleChart,
-        ActionButton
+        Container
     }
 }
 </script>
