0b1cc29b2bd6de8e32d51097b583f40c154ecf34
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-23 23:36:14 +0100

    create Token

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -16,46 +16,6 @@ export const formatAddress = (address) => {
     return `${address.slice(0, 14)}...${address.slice(-5)}`;
 }
 
-export const createWallet = ({ chain_id, address, items, name, logo_url }) => {
-    const key = `${chain_id.toString()}_${address}`;
-    let wallet = {
-        key,
-        uuid: uuidv4(key),
-        name,
-        fiat_balance: 0,
-        chain_id,
-        address: {
-            full: address,
-            truncated: formatAddress(address)
-        },
-        logo_url,
-        tokens_count: 0
-    };
-    items.forEach(item => {
-        // update wallet's fiat balance
-        wallet.fiat_balance += item.quote;
-        wallet.tokens_count++;
-    });
-    wallet.fiat_balance = formatFiatValue(wallet.fiat_balance);
-    return wallet;
-}
-
-export const createToken = ({ contract_decimals, contract_address, contract_name, contract_ticker_symbol, quote, balance, supports_erc, logo_url }) => {
-    let token = {
-        contract_address: {
-            full: contract_address,
-            truncated: formatAddress(contract_address)
-        },
-        contract_name,
-        contract_ticker_symbol,
-        logo_url,
-        balance: formatTokenBalance(balance, contract_decimals),
-        fiat_balance: formatFiatValue(quote),
-        erc_codes: supports_erc ? supports_erc.map(item => item.split('erc')[1]) : null
-    }
-    return token;
-}
-
 export const createHistoryGraphData = ({ holdings, contract_decimals }) => {
     const line = [],
         candlestick = [];
---
+++ src/types/Token.js
@@ -0,0 +1,26 @@
+import {formatAddress, formatTokenBalance, formatFiatValue} from '../lib/helpers';
+
+export const Token = ({
+    contract_decimals,
+    contract_address,
+    contract_name,
+    contract_ticker_symbol,
+    quote,
+    balance,
+    supports_erc,
+    logo_url
+}) => {
+    let token = {
+        contract_address: {
+            full: contract_address,
+            truncated: formatAddress(contract_address)
+        },
+        contract_name,
+        contract_ticker_symbol,
+        logo_url,
+        balance: formatTokenBalance(balance, contract_decimals),
+        fiat_balance: formatFiatValue(quote),
+        erc_codes: supports_erc ? supports_erc.map(item => item.split('erc')[1]) : null
+    }
+    return token;
+}
