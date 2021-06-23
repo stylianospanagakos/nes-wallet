c1efef60b981ea0ad07865bc9d7f3d93d1b8ebce
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-23 23:41:04 +0100

    add Transaction

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,3 @@
-import moment from 'moment';
-
 export const formatTokenBalance = (balance, decimals) => {
     const value = decimals > 0 ?
         parseInt(balance) / Math.pow(10, decimals) :
@@ -45,20 +43,4 @@ export const createHistoryGraphData = ({ holdings, contract_decimals }) => {
         line: line.reverse(),
         candlestick: candlestick.reverse()
     };
-}
-
-export const createTransaction = ({ block_signed_at, gas_offered, gas_price, gas_spent, successful, value, from_address, to_address, transfer_type, tx_hash, contract_decimals }) => {
-    return {
-        amount: formatTokenBalance(value, contract_decimals),
-        successful,
-        block_signed_at: moment(block_signed_at).format('D MMM YY HH:mm'),
-        gas_offered,
-        gas_price,
-        gas_spent,
-        from_address,
-        to_address,
-        transfer_type,
-        display_address: formatAddress(to_address),
-        tx_hash
-    }
 }
---
+++ src/types/Transaction.js
@@ -0,0 +1,30 @@
+import {formatTokenBalance, formatAddress} from '../lib/helpers';
+import moment from 'moment';
+
+export const Transaction = ({
+    block_signed_at,
+    gas_offered,
+    gas_price,
+    gas_spent,
+    successful,
+    value,
+    from_address,
+    to_address,
+    transfer_type,
+    tx_hash,
+    contract_decimals
+}) => {
+    return {
+        amount: formatTokenBalance(value, contract_decimals),
+        successful,
+        block_signed_at: moment(block_signed_at).format('D MMM YY HH:mm'),
+        gas_offered,
+        gas_price,
+        gas_spent,
+        from_address,
+        to_address,
+        transfer_type,
+        display_address: formatAddress(to_address),
+        tx_hash
+    }
+}
