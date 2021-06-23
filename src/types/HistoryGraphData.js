9013188f6a124b396e468bd46f2769b0edbe4b63
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-23 23:46:29 +0100

    add HistoryGraphData

---
+++ src/types/HistoryGraphData.js
@@ -0,0 +1,33 @@
+import {formatTokenBalance} from '../lib/helpers';
+
+export const HistoryGraphData = ({ holdings, contract_decimals }) => {
+    const line = [],
+        candlestick = [];
+    holdings.forEach(({timestamp, open, high, low, close}) => {
+        let date = new Date(timestamp),
+            openFormatted = formatTokenBalance(open.balance, contract_decimals),
+            highFormatted = formatTokenBalance(high.balance, contract_decimals),
+            lowFormatted = formatTokenBalance(low.balance, contract_decimals),
+            closeFormatted = formatTokenBalance(close.balance, contract_decimals);
+
+        /**
+         * Line chart format [{ x: date, y: 76 }]
+         */
+        line.push({
+            x: date,
+            y: closeFormatted
+        });
+        /**
+         * Candlestick chart format: [{ x: date, y: [O,H,L,C] }]
+         */
+         candlestick.push({
+            x: date,
+            y: [openFormatted, highFormatted, lowFormatted, closeFormatted]
+        });
+    });
+    // reverse order of history items
+    return {
+        line: line.reverse(),
+        candlestick: candlestick.reverse()
+    };
+}
