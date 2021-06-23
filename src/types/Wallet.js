575130e2d7f9477d334ccaf21a3d47398a964ee4
Stylianos Panagakos <stylianos.panagakos@outlook.com>
2021-06-23 23:34:38 +0100

    add Wallet type

---
+++ src/types/Wallet.js
@@ -0,0 +1,25 @@
+import {formatAddress, formatFiatValue} from '../lib/helpers';
+
+export const Wallet = ({ chain_id, address, items, name, logo_url }) => {
+    const key = `${chain_id.toString()}_${address}`;
+    let wallet = {
+        key,
+        uuid: uuidv4(key),
+        name,
+        fiat_balance: 0,
+        chain_id,
+        address: {
+            full: address,
+            truncated: formatAddress(address)
+        },
+        logo_url,
+        tokens_count: 0
+    };
+    items.forEach(item => {
+        // update wallet's fiat balance
+        wallet.fiat_balance += item.quote;
+        wallet.tokens_count++;
+    });
+    wallet.fiat_balance = formatFiatValue(wallet.fiat_balance);
+    return wallet;
+};
